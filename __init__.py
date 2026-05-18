792fc4547a25057b3a46b195b74cb2e337fa18ca
unh1nge <trashandburn420@gmail.com>
2026-05-18 12:58:33 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
-from .comfyui_character_composer import *
+"""ComfyUI Character Composer custom node package."""
+
+from .comfyui_character_composer import NODE_CLASS_MAPPINGS, ComfyUICharacterComposer
+
+WEB_DIRECTORY = "./web"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "ComfyUICharacterComposer", "WEB_DIRECTORY"]
